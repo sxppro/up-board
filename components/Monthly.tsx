870c34a036932f1063a8cac2c4c2e5754dd21d8d
Josh <j.p.lu@outlook.com>
2023-08-08 22:32:27 +1000

    feat: update monthly view again

--- components/Monthly.tsx
+++ components/Monthly.tsx
@@ -1,57 +1,61 @@
 'use client';
 import { MonthlyMetric } from '@/types/custom';
-import { useYearlyTransactions } from '@/utils/fetch';
+import { useMonthlyMetrics } from '@/utils/fetch';
+import { formatCurrency } from '@/utils/helpers';
 import { BarChart, Card, Text, Title } from '@tremor/react';
 
 const data = [
   {
     Month: 'Jan 21',
     Income: 2890,
-    Spending: 2400,
+    Expenses: 2400,
   },
   {
     Month: 'Feb 21',
-    income: 1890,
-    spending: 1398,
+    Income: 1890,
+    Expenses: 1398,
   },
   // ...
   {
     Month: 'Jan 22',
-    income: 3890,
-    spending: 2980,
+    Income: 3890,
+    Expenses: 2980,
   },
 ];
 
+const currentDate = new Date();
+
 const Monthly = () => {
-  const currentDate = new Date('2022-12-31T12:59:59.999Z');
-  const { data, isLoading } = useYearlyTransactions(currentDate);
+  const { data, isLoading } = useMonthlyMetrics(currentDate);
 
   const metrics =
     !isLoading &&
     data &&
-    data.map(({ Month, Year, ...rest }: MonthlyMetric) => ({
-      ...rest,
-      Year,
-      Month: `${new Date(Year, Month).toLocaleDateString('default', {
-        month: 'long',
-      })} ${Year}`,
-    }));
+    data.map(({ Month, Year, ...rest }: MonthlyMetric) => {
+      const date = new Date(Year, Month);
+      return {
+        ...rest,
+        Time: `${date.toLocaleDateString('default', {
+          month: 'short',
+        })} ${date.toLocaleDateString('default', {
+          year: '2-digit',
+        })}`,
+      };
+    });
 
   return (
     <Card>
-      <Title>Performance</Title>
-      <Text>Comparison between Money Income and Expenses</Text>
+      <Title>Monthly Comparison</Title>
+      <Text>Total income and expenses by month</Text>
       <BarChart
         className="mt-4 h-80"
         data={metrics}
-        index="Month"
+        index="Time"
         categories={['Income', 'Expenses']}
         colors={['indigo', 'fuchsia']}
         stack={false}
         yAxisWidth={60}
-        valueFormatter={(number: number) =>
-          `$ ${Intl.NumberFormat('us').format(number).toString()}`
-        }
+        valueFormatter={(number: number) => formatCurrency(number)}
       />
     </Card>
   );
