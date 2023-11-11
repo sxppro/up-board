ccb43755a7ca2712aabaf686fb5dca38ad190314
Josh <j.p.lu@outlook.com>
2023-11-11 23:52:56 +1100

    feat: add login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import BasicDashboard from '@/components/BasicDashboard';
+import { authOptions } from '@/utils/auth';
+import { getServerSession } from 'next-auth';
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+
   return <BasicDashboard />;
 };
 
