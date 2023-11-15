f7fb949ad11df526b80c48f890de0bcb76309b7d
Josh <j.p.lu@outlook.com>
2023-11-15 23:42:32 +1100

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from '@/components/ui/toaster';
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -37,6 +38,7 @@ export default async function RootLayout({
       <body>
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
