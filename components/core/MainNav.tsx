a04345890bd940a73416fbcc75adc35ccd61f1fc
Josh <j.p.lu@outlook.com>
2023-11-19 22:55:42 +1100

    feat: add nav link that respond to url

--- components/core/MainNav.tsx
+++ components/core/MainNav.tsx
@@ -1,5 +1,5 @@
 import { cn } from '@/utils/helpers';
-import Link from 'next/link';
+import ActiveLink from './ActiveLink';
 
 export function MainNav({
   className,
@@ -10,18 +10,8 @@ export function MainNav({
       className={cn('flex items-center space-x-4 lg:space-x-6', className)}
       {...props}
     >
-      <Link
-        href="/"
-        className="text-sm font-medium transition-colors hover:text-primary"
-      >
-        Overview
-      </Link>
-      <Link
-        href="/transactions"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        Transactions
-      </Link>
+      <ActiveLink href="/">Overview</ActiveLink>
+      <ActiveLink href="/transactions">Transactions</ActiveLink>
     </nav>
   );
 }
