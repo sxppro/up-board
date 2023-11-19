2dc1c3e237351d3389016310b44949ce618ce7be
Josh <j.p.lu@outlook.com>
2023-11-19 23:51:54 +1100

    feat: date check to transactions endpoint

--- app/api/transactions/route.ts
+++ app/api/transactions/route.ts
@@ -16,10 +16,27 @@ export async function GET(request: NextRequest) {
     );
   }
 
+  /**
+   * Check date validity
+   */
+  if (
+    isNaN(
+      new Date(request.nextUrl.searchParams.get('start') || '').getTime()
+    ) ||
+    isNaN(new Date(request.nextUrl.searchParams.get('end') || '').getTime())
+  ) {
+    return NextResponse.json(
+      {
+        error: 'Bad Request',
+      },
+      { status: 400 }
+    );
+  }
+
   return NextResponse.json({
     data: await getTransactionsByDate(
-      new Date(request.nextUrl.searchParams.get('start') || ''),
-      new Date(request.nextUrl.searchParams.get('end') || '')
+      new Date(request.nextUrl.searchParams.get('start') as string),
+      new Date(request.nextUrl.searchParams.get('end') as string)
     ),
   });
 }
