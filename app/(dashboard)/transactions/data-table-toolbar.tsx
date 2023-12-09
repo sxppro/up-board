da97a9160de53fd4ce94e82f71262c3c66d136f8
Josh <j.p.lu@outlook.com>
2023-12-10 00:30:49 +1100

    feat: add reset button

--- app/(dashboard)/transactions/data-table-toolbar.tsx
+++ app/(dashboard)/transactions/data-table-toolbar.tsx
@@ -1,6 +1,8 @@
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { CategoryOption } from '@/types/custom';
 import { Table } from '@tanstack/react-table';
+import { X } from 'lucide-react';
 import { DataTableFacetedFilter } from './data-table-faceted-filter';
 import { DataTableViewOptions } from './data-table-view-options';
 
@@ -13,6 +15,8 @@ export function DataTableToolbar<TData>({
   table,
   options,
 }: DataTableToolbarProps<TData>) {
+  const isFiltered = table.getState().columnFilters.length > 0;
+
   return (
     <div className="flex flex-col sm:flex-row items-center justify-between gap-2">
       <div className="flex flex-1 items-center space-x-2 w-full">
@@ -32,6 +36,15 @@ export function DataTableToolbar<TData>({
             title="Category"
             options={options}
           />
+        )}
+        {isFiltered && (
+          <Button
+            variant="ghost"
+            onClick={() => table.resetColumnFilters()}
+            className="h-8"
+          >
+            Reset <X className="ml-2 h-4 w-4" />
+          </Button>
         )}
       </div>
       <DataTableViewOptions table={table} />
