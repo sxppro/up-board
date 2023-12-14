9c6c49eaff328fac505bb33836d3696cbacea00a
Josh <j.p.lu@outlook.com>
2023-12-14 12:27:18 +1100

    feat: update transactions table to hit db

--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -1,12 +1,14 @@
 import { columns } from '@/app/(dashboard)/transactions/columns';
 import { DataTable } from '@/app/(dashboard)/transactions/data-table';
-import { getChildCategories } from '@/db';
-import { getTransactionsByDate } from '@/utils/server';
+import { getChildCategories, getTransactionsByDate } from '@/db';
+import { filterTransactionFields } from '@/utils/helpers';
 import { subMonths } from 'date-fns';
 
 const TransactionsTable = async () => {
   const now = new Date();
-  const data = await getTransactionsByDate(subMonths(now, 1), now);
+  const data = filterTransactionFields(
+    await getTransactionsByDate(subMonths(now, 1), now)
+  );
   const categories = await getChildCategories();
 
   return <DataTable columns={columns} data={data} options={categories} />;
