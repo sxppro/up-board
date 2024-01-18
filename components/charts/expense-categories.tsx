e7279b314c1f5a1e87f09023446f83f42a23b001
Josh <j.p.lu@outlook.com>
2024-01-18 13:49:26 +1100

    fix: add loading indicator to expense category legend

--- components/charts/expense-categories.tsx
+++ components/charts/expense-categories.tsx
@@ -4,6 +4,7 @@ import { formatCurrency } from '@/utils/helpers';
 import { useCategoryMetrics, useDate } from '@/utils/hooks';
 import { DonutChart, Legend, Title } from '@tremor/react';
 import DashboardCard from '../core/dashboard-card';
+import { Skeleton } from '../ui/skeleton';
 
 const ExpenseCategories = () => {
   const { date } = useDate();
@@ -16,13 +17,18 @@ const ExpenseCategories = () => {
   return (
     <DashboardCard>
       <Title>Expense Categories</Title>
-      <Legend
-        categories={
-          isLoading || !data
-            ? []
-            : data.map(({ category }: { category: string }) => category)
-        }
-      />
+      {isLoading || !data ? (
+        <>
+          <Skeleton className="w-full h-4" />
+          <Skeleton className="w-full h-4" />
+        </>
+      ) : (
+        <Legend
+          categories={data.map(
+            ({ category }: { category: string }) => category
+          )}
+        />
+      )}
       <DonutChart
         className="h-80"
         data={data}
