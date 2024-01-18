aad372bc16209eaf6700b54a994c80e12eaf7b10
Josh <j.p.lu@outlook.com>
2024-01-18 12:54:11 +1100

    feat: add/remove tags to transaction using combobox

--- components/tables/transaction-details.tsx
+++ components/tables/transaction-details.tsx
@@ -1,9 +1,10 @@
+'use client';
+
 import { formatCurrency } from '@/utils/helpers';
-import { useTransaction } from '@/utils/hooks';
-import { Plus } from 'lucide-react';
+import { useTags, useTransaction } from '@/utils/hooks';
 import TableSkeleton from '../TableSkeleton';
+import TransactionTagsCombobox from '../core/transaction-tags-combobox';
 import { Badge } from '../ui/badge';
-import { Button } from '../ui/button';
 import {
   Table,
   TableBody,
@@ -17,52 +18,60 @@ interface TransactionDetailsProps {
 }
 
 const TransactionDetails = ({ transactionId }: TransactionDetailsProps) => {
-  const { data, isLoading } = useTransaction(transactionId);
+  const { data: txDetails, isLoading } = useTransaction(transactionId);
+  const { data: tags } = useTags();
+  const isTagged =
+    txDetails && Array.isArray(txDetails.tags) && txDetails.tags.length > 0;
 
-  return isLoading || !data ? (
+  return isLoading || !txDetails ? (
     <TableSkeleton cols={2} rows={7} />
   ) : (
     <Table>
-      <TableCaption>{`Transaction ID: ${data.id}`}</TableCaption>
+      <TableCaption>{`Transaction ID: ${txDetails.id}`}</TableCaption>
       <TableBody>
         <TableRow>
           <TableCell className="font-medium">Description</TableCell>
-          <TableCell className="text-end">{data.description}</TableCell>
+          <TableCell className="text-end">{txDetails.description}</TableCell>
         </TableRow>
         <TableRow>
           <TableCell className="font-medium">Amount</TableCell>
           <TableCell className="text-end">
-            {formatCurrency(data.amountRaw)}
+            {formatCurrency(txDetails.amountRaw)}
           </TableCell>
         </TableRow>
         <TableRow>
           <TableCell className="font-medium">Time</TableCell>
           <TableCell className="text-end">
-            {new Date(data.time).toLocaleString()}
+            {new Date(txDetails.time).toLocaleString()}
           </TableCell>
         </TableRow>
         <TableRow>
           <TableCell className="font-medium">Category</TableCell>
-          <TableCell className="text-end">{data.category}</TableCell>
+          <TableCell className="text-end">{txDetails.category}</TableCell>
         </TableRow>
         <TableRow>
           <TableCell className="font-medium">Parent Category</TableCell>
-          <TableCell className="text-end">{data.parentCategory}</TableCell>
+          <TableCell className="text-end">{txDetails.parentCategory}</TableCell>
         </TableRow>
         <TableRow>
           <TableCell className="font-medium">Status</TableCell>
-          <TableCell className="text-end">{data.status}</TableCell>
+          <TableCell className="text-end">{txDetails.status}</TableCell>
         </TableRow>
         <TableRow>
           <TableCell className="font-medium">Tags</TableCell>
           <TableCell className="text-end">
-            {Array.isArray(data.tags) && data.tags.length > 0 ? (
-              data.tags.map((tag: string) => <Badge key={tag}>{tag}</Badge>)
-            ) : (
-              <Button size="sm" variant="outline" className="h-6 text-xs">
-                <Plus className="mr-1 h-3 w-3" /> Add tag
-              </Button>
-            )}
+            <div className="flex flex-wrap justify-end gap-1">
+              {isTagged &&
+                txDetails.tags.map((tag: string) => (
+                  <Badge key={tag}>{tag}</Badge>
+                ))}
+              <TransactionTagsCombobox
+                txId={txDetails.id}
+                title="Add tags"
+                options={tags || []}
+                initialState={txDetails.tags}
+              />
+            </div>
           </TableCell>
         </TableRow>
       </TableBody>
