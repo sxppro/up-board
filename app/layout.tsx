e640e193d0b74eabf556f16710f02b46bbcf4d20
Josh <j.p.lu@outlook.com>
2024-01-20 00:23:51 +1100

    fix: theme provider file location

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import ThemeProvider from '@/components/core/theme-provider';
+import ThemeProvider from '@/components/providers/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
