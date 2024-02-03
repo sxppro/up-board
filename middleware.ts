175ed6cf0394c4dc879c40e03257863f2f085030
Josh <j.p.lu@outlook.com>
2024-02-03 14:44:27 +1100

    fix: update middleware

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,5 @@ import { withAuth } from 'next-auth/middleware';
 export default withAuth;
 
 export const config = {
-  matcher: ['/', '/transactions'],
+  matcher: ['/', '/transactions', '/tag/:path*'],
 };
