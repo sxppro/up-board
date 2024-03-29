1eea883c7d5617a15a615cc649545e84e69c0427
Josh <j.p.lu@outlook.com>
2024-03-30 01:17:59 +1100

    fix: add tags page to middleware

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,5 @@ import { withAuth } from 'next-auth/middleware';
 export default withAuth;
 
 export const config = {
-  matcher: ['/', '/transactions', '/tag/:path*'],
+  matcher: ['/', '/transactions', '/tag/:path*', '/tags'],
 };
