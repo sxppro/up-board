5b027c0cf3680809a4aa5f50499f9bbd9fbe61cb
Josh <j.p.lu@outlook.com>
2024-04-04 14:32:16 +1100

    feat: add option to maintain query params in link
    helper fn to flatten query param arrays

--- components/core/active-link.tsx
+++ components/core/active-link.tsx
@@ -2,24 +2,32 @@
 
 import { cn } from '@/utils/helpers';
 import Link, { LinkProps } from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 import { PropsWithChildren } from 'react';
+
+type ActiveLinkProps = {
+  keepQueryParams?: boolean;
+} & LinkProps &
+  PropsWithChildren;
 
 /**
  * Checks whether current page matches link
+ * @param keepQueryParams whether to maintain query params in URL
  * @returns
  */
 const ActiveLink = ({
   children,
   href,
+  keepQueryParams = false,
   ...rest
-}: PropsWithChildren & LinkProps) => {
+}: ActiveLinkProps) => {
   const pathName = usePathname();
+  const searchParams = useSearchParams();
   const isActive = pathName === href;
   return (
     <Link
       {...rest}
-      href={href}
+      href={keepQueryParams ? `${href}?${searchParams}` : href}
       className={cn(
         `text-sm font-medium transition-colors hover:text-primary`,
         isActive ? '' : 'text-muted-foreground'
