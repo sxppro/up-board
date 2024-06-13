fbbc7629c710460267662758efd1e6eebc418fb8
Josh <j.p.lu@outlook.com>
2024-06-13 12:21:19 +1000

    chore: add account charts layout idea

--- app/(dashboard)/account/[accountId]/charts.tsx
+++ app/(dashboard)/account/[accountId]/charts.tsx
@@ -1,5 +1,6 @@
 import IOBar from '@/components/charts/io-bar';
-import { Col, Grid, Title } from '@tremor/react';
+import DashboardCard from '@/components/core/dashboard-card';
+import { Col, Grid, Text, Title } from '@tremor/react';
 
 interface AccountChartsProps {
   accountId: string;
@@ -7,14 +8,51 @@ interface AccountChartsProps {
 
 const AccountCharts = ({ accountId }: AccountChartsProps) => {
   return (
-    <Grid numItemsMd={2} numItemsLg={3} className="gap-4">
-      <Col>
-        <IOBar accountId={accountId}>
-          <Title>Today</Title>
-        </IOBar>
-      </Col>
-      <Col></Col>
-    </Grid>
+    <>
+      <Grid numItemsMd={2} numItemsLg={3} className="gap-4">
+        <Col>
+          <DashboardCard>
+            <Text>
+              Income tx + show more button that takes u to
+              /transactions?accountId=... OR dialog of filtered transactions (by
+              acc, income, date range)
+            </Text>
+          </DashboardCard>
+        </Col>
+        <Col>
+          <DashboardCard>
+            <Text>Expenses tx</Text>
+          </DashboardCard>
+        </Col>
+        <Col>
+          <IOBar accountId={accountId}>
+            <Title>Today</Title>
+          </IOBar>
+        </Col>
+      </Grid>
+      <Grid numItemsMd={2} className="gap-4">
+        <Col>
+          <DashboardCard>
+            <Text>Good Life</Text>
+          </DashboardCard>
+        </Col>
+        <Col>
+          <DashboardCard>
+            <Text>Personal</Text>
+          </DashboardCard>
+        </Col>
+        <Col>
+          <DashboardCard>
+            <Text>Home</Text>
+          </DashboardCard>
+        </Col>
+        <Col>
+          <DashboardCard>
+            <Text>Transport</Text>
+          </DashboardCard>
+        </Col>
+      </Grid>
+    </>
   );
 };
 
