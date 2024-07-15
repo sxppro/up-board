3ff7bca4d16e46fac1aa5facefc242bd904028a0
Josh <j.p.lu@outlook.com>
2024-07-16 00:20:16 +1000

    feat: use limit in trpc query to fetch transactions
    although RecentTransactions is used in deprecated overview page

--- components/tables/recent-transactions.tsx
+++ components/tables/recent-transactions.tsx
@@ -1,22 +1,23 @@
 'use client';
 
-import { FilteredTransactionResource } from '@/types/custom';
 import { formatCurrency } from '@/utils/helpers';
 import { useDate } from '@/utils/hooks';
 import { trpc } from '@/utils/trpc';
 import {
+  Flex,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableHeaderCell,
   TableRow,
+  Text,
   Title,
 } from '@tremor/react';
 import DashboardCard from '../core/dashboard-card';
 import TableSkeleton from '../core/table-skeleton';
 
-const ExpenseTransactions = () => {
+const RecentTransactions = () => {
   const { date } = useDate();
   const { data, isLoading } = trpc.user.getTransactionsByDate.useQuery({
     dateRange: { from: date?.from, to: date?.to },
@@ -24,6 +25,7 @@ const ExpenseTransactions = () => {
     type: 'transactions',
     sort: 'time',
     sortDir: 'desc',
+    limit: 6,
   });
 
   return (
@@ -31,7 +33,7 @@ const ExpenseTransactions = () => {
       <Title>Transactions</Title>
       {isLoading || !data ? (
         <TableSkeleton cols={2} rows={7} />
-      ) : (
+      ) : data.length > 0 ? (
         <Table className="w-full flex-1">
           <TableHead>
             <TableRow>
@@ -40,29 +42,25 @@ const ExpenseTransactions = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {data
-              .slice(0, 6)
-              .map(
-                ({
-                  id,
-                  description,
-                  amountRaw,
-                }: FilteredTransactionResource) => (
-                  <TableRow key={id}>
-                    <TableCell className="max-w-[225px] truncate">
-                      {description}
-                    </TableCell>
-                    <TableCell className="text-end">
-                      {formatCurrency(amountRaw)}
-                    </TableCell>
-                  </TableRow>
-                )
-              )}
+            {data.map(({ id, description, amountRaw }) => (
+              <TableRow key={id}>
+                <TableCell className="max-w-[225px] truncate">
+                  {description}
+                </TableCell>
+                <TableCell className="text-end">
+                  {formatCurrency(amountRaw)}
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
+      ) : (
+        <Flex className="justify-center items-center w-full h-full border border-dashed rounded-tremor-default border-tremor-border dark:border-dark-tremor-border">
+          <Text>No transactions</Text>
+        </Flex>
       )}
     </DashboardCard>
   );
 };
 
-export default ExpenseTransactions;
+export default RecentTransactions;
