257eb1ec0c136d4dd3ca6038d9437ff9ff72fe94
Josh <j.p.lu@outlook.com>
2024-08-25 21:48:13 +1000

    fix: middleware:

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,7 @@ import { withAuth } from 'next-auth/middleware';
 export default withAuth;
 
 export const config = {
-  matcher: ['/((?!api|login|a|e|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!api|login|a|e|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|manifest.webmanifest).*)',
+  ],
 };
