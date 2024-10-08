8d342fed6d551a2b4daf3f50d6219f885b763a38
Josh <j.p.lu@outlook.com>
2024-10-09 01:06:45 +1100

    chore: update mocks

--- server/schemas.ts
+++ server/schemas.ts
@@ -96,7 +96,7 @@ export const TransactionResourceFilteredSchema = z.object({
   category: z.string(),
   parentCategory: z.string(),
   tags: z.string().array(),
-  deepLinkURL: z.string(),
+  deepLinkURL: z.string().optional(),
 });
 export type TransactionResourceFiltered = z.infer<
   typeof TransactionResourceFilteredSchema
