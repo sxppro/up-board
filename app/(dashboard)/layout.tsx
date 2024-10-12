27a2b5fe3bd476f71792c5d29ef7c283c7f8a9fd
Josh <j.p.lu@outlook.com>
2024-10-13 01:26:44 +1100

    style: dashboard layout padding

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -19,7 +19,13 @@ export default async function DashboardLayout({
       <PageProgressBar />
       <div className="mx-auto max-w-screen-2xl">
         <Sidebar user={user} />
-        <main className="lg:pl-72">{children}</main>
+        <main className="lg:pl-72">
+          <div className="relative">
+            <div className="flex flex-col p-4 sm:px-6 sm:pb-10 sm:pt-10 lg:px-10 lg:pt-7">
+              {children}
+            </div>
+          </div>
+        </main>
       </div>
     </>
   );
