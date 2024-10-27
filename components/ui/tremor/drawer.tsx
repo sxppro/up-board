fed7dbbddf7a768fac8677e9c0bca50d12fc9df2
Josh <j.p.lu@outlook.com>
2024-10-28 03:07:21 +1100

    style: shadcn-ify tremor drawer

--- components/ui/tremor/drawer.tsx
+++ components/ui/tremor/drawer.tsx
@@ -49,7 +49,7 @@ const DrawerOverlay = React.forwardRef<
         // base
         'fixed inset-0 z-50 overflow-y-auto',
         // background color
-        'bg-black/30',
+        'bg-background/80 backdrop-blur-sm',
         // transition
         'data-[state=closed]:animate-hide data-[state=open]:animate-dialogOverlayShow',
         className
@@ -80,7 +80,7 @@ const DrawerContent = React.forwardRef<
             // border color
             'border-gray-200 dark:border-gray-900',
             // background color
-            'bg-white dark:bg-[#090E1A]',
+            'bg-primary-foreground',
             // transition
             'data-[state=closed]:animate-drawerSlideRightAndFade data-[state=open]:animate-drawerSlideLeftAndFade',
             focusRing,
