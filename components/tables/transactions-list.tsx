8059099bc6eba95ac2c9987d29d20079c6c2edbb
Josh <j.p.lu@outlook.com>
2024-11-11 02:39:53 +1100

    feat: little icons to show tags, notes and attachments

--- components/tables/transactions-list.tsx
+++ components/tables/transactions-list.tsx
@@ -5,6 +5,7 @@ import {
   outputTransactionFields,
 } from '@/utils/helpers';
 import { focusRing } from '@/utils/tremor';
+import { Note, Paperclip, Tag } from '@phosphor-icons/react/dist/ssr';
 import TransactionPopover from '../transaction-popover';
 import { Button } from '../ui/button';
 import { Separator } from '../ui/separator';
@@ -31,18 +32,29 @@ const TransactionsList = ({ transactions }: TransactionsListProps) => {
       <p className="text-lg font-bold">{formatDate(timestamp)}</p>
       <Separator className="mt-1" />
       <div className="flex flex-col">
-        {transactions.map(({ id, attributes }) => (
+        {transactions.map(({ id, attributes, relationships }) => (
           <TransactionPopover key={id} id={id}>
             <Button
               className={cn(
-                'h-8 flex -mx-3 items-center text-sm font-medium overflow-hidden rounded-lg hover:bg-muted/80',
+                'h-8 flex -mx-3 items-center justify-between gap-2 text-sm font-medium rounded-lg hover:bg-muted/80',
                 focusRing
               )}
               variant="ghost"
             >
-              <p className="flex-1 text-left text-subtle truncate">
-                {attributes.description}
-              </p>
+              <div className="flex items-center gap-1 overflow-hidden">
+                <p className="text-left text-subtle truncate">
+                  {attributes.description}
+                </p>
+                {relationships.tags.data.length > 0 && (
+                  <Tag className="size-4 fill-fuchsia-600" />
+                )}
+                {attributes.note?.text && (
+                  <Note className="size-4 fill-lime-600" />
+                )}
+                {relationships.attachment.data?.id && (
+                  <Paperclip className="size-4 fill-rose-600" />
+                )}
+              </div>
               <span>
                 {formatCurrency(attributes.amount.valueInBaseUnits / 100)}
               </span>
