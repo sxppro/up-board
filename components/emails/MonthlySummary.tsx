8ecf66e3afadab99332bf2fada1e06c61a4b0e92
Josh <j.p.lu@outlook.com>
2025-04-13 22:42:53 +1000

    feat: update spend grid tiles

--- components/emails/MonthlySummary.tsx
+++ components/emails/MonthlySummary.tsx
@@ -1,4 +1,7 @@
 import tailwindConfig from '@/tailwind.config';
+import { AvailableChartColorsKeys, getColorClassName } from '@/utils/charts';
+import { colours } from '@/utils/constants';
+import { cn, formatCurrency } from '@/utils/helpers';
 import {
   Body,
   Column,
@@ -14,46 +17,46 @@ import {
   Text,
 } from '@react-email/components';
 
-const MonthlySummary = () => {
+interface MonthlySummaryProps {
+  dateRangeText: string; // e.g. March 2025
+}
+interface SpendGridItemProps {
+  category: string; // Category name
+  spending?: number; // Amount spent in this category
+  totalSpending?: number; // Total amount spent over all categories
+}
+
+const MonthlySummary = ({ dateRangeText }: MonthlySummaryProps) => {
   return (
     <Html lang="en">
-      <Head />
-      <Preview>Up Board — March 2025</Preview>
       <Tailwind config={tailwindConfig}>
+        <Head />
+        <Preview>Up Board — March 2025</Preview>
         <Body className="bg-slate-50 text-slate-900 font-sans">
-          <Container className="mx-auto w-full max-w-[600px] p-8">
+          <Container className="mx-auto w-full max-w-[600px] p-6">
             {/* Heading */}
             <Section className="text-center">
               <Text className="mx-0 my-4 p-0 text-2xl font-bold tracking-tight">
                 Up Board
               </Text>
               <Hr className="my-2" />
-              <Heading className="my-4 text-4xl font-medium leading-tight">
+              <Heading className="my-4 text-6xl font-medium leading-tight">
                 Your Month in Review
               </Heading>
               <Text className="inline-block my-1 bg-fuchsia-600 text-slate-50 text-sm font-normal uppercase tracking-wider rounded-full px-4 py-1">
-                March 2025
+                {dateRangeText}
               </Text>
             </Section>
 
             {/* Spend grid */}
             <Section className="my-4">
-              <Row cellSpacing={16} cellPadding={8}>
-                <Column className="h-[200px] w-1/2 border border-solid border-slate-200 rounded-2xl shadow">
-                  <div className="h-full flex flex-col justify-end gap-4">
-                    <Text className="text-3xl font-medium">$456.56</Text>
-                    <Text className="text-lg">Good Life</Text>
-                  </div>
-                </Column>
-                <Column className="h-[200px] w-1/2 border border-solid border-slate-200 rounded-2xl shadow">
-                  <Text>Test</Text>
-                </Column>
+              <Row cellSpacing={16} cellPadding={12}>
+                <SpendGridItem category="Good Life" />
+                <SpendGridItem category="Personal" />
               </Row>
-              <Row cellSpacing={16} cellPadding={8} className="-mt-4">
-                <Column className="h-[200px] w-1/2 border border-solid border-slate-200 rounded-2xl shadow">
-                  <Text>Test</Text>
-                </Column>
-                <Column className="h-[200px] w-1/2 border border-solid border-slate-200 rounded-2xl shadow"></Column>
+              <Row cellSpacing={16} cellPadding={12} className="-mt-4">
+                <SpendGridItem category="Home" />
+                <SpendGridItem category="Transport" />
               </Row>
             </Section>
 
@@ -71,6 +74,44 @@ const MonthlySummary = () => {
       </Tailwind>
     </Html>
   );
+};
+
+const SpendGridItem = ({
+  category,
+  spending = 0,
+  totalSpending = spending,
+}: SpendGridItemProps) => {
+  const barColor = colours[category] ?? 'gray';
+  const percentage = (spending / totalSpending) * 100;
+  return (
+    <Column className="w-1/2 sm:pt-24 border border-solid border-slate-200 rounded-2xl shadow">
+      <div className="h-full flex flex-col justify-end gap-2">
+        <Text className="text-4xl font-medium m-0">
+          {formatCurrency(spending)}
+        </Text>
+        <Text className="text-lg m-0">{category}</Text>
+        {spending === 0 ? (
+          <div
+            className="h-2 rounded-full border border-dashed border-gray-500"
+            style={{ width: '100%' }}
+          />
+        ) : (
+          <div
+            className={cn(
+              'h-2 rounded-full',
+              getColorClassName(barColor as AvailableChartColorsKeys, 'bg'),
+              percentage === 0 && 'hidden'
+            )}
+            style={{ width: `${percentage}%` }}
+          />
+        )}
+      </div>
+    </Column>
+  );
+};
+
+MonthlySummary.PreviewProps = {
+  dateRangeText: 'March 2025',
 };
 
 export default MonthlySummary;
