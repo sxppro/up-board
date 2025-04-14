863b47fb903e4683cc82895a4c57ef04a9eb9da3
Josh <j.p.lu@outlook.com>
2025-04-14 21:33:31 +1000

    feat: add merchant and subcategory spending to email

--- components/emails/MonthlySummary.tsx
+++ components/emails/MonthlySummary.tsx
@@ -1,7 +1,8 @@
+import { Merchant, TransactionCategoryInfo } from '@/server/schemas';
 import tailwindConfig from '@/tailwind.config';
 import { AvailableChartColorsKeys, getColorClassName } from '@/utils/charts';
 import { colours } from '@/utils/constants';
-import { cn, formatCurrency } from '@/utils/helpers';
+import { cn, formatCurrency, formatCurrencyAbsolute } from '@/utils/helpers';
 import {
   Body,
   Column,
@@ -19,14 +20,30 @@ import {
 
 interface MonthlySummaryProps {
   dateRangeText: string; // e.g. March 2025
+  categorySpending: TransactionCategoryInfo[]; // Top spending categories
+  merchantSpending: Merchant[]; // Top spending merchants
 }
-interface SpendGridItemProps {
+
+interface SpendBarProps {
+  percentage: number;
+  spending: number;
+  barColor?: string;
+}
+
+interface SpendGridTileProps {
   category: string; // Category name
   spending?: number; // Amount spent in this category
   totalSpending?: number; // Total amount spent over all categories
 }
 
-const MonthlySummary = ({ dateRangeText }: MonthlySummaryProps) => {
+const MonthlySummary = ({
+  dateRangeText,
+  categorySpending,
+  merchantSpending,
+}: MonthlySummaryProps) => {
+  // ! Assumes spending is sorted by absAmount descending
+  const categorySpendingMax = categorySpending[0];
+  const merchantSpendingMax = merchantSpending[0];
   return (
     <Html lang="en">
       <Tailwind config={tailwindConfig}>
@@ -40,7 +57,7 @@ const MonthlySummary = ({ dateRangeText }: MonthlySummaryProps) => {
                 Up Board
               </Text>
               <Hr className="my-2" />
-              <Heading className="my-4 text-6xl font-medium leading-tight">
+              <Heading className="my-4 text-4xl font-medium leading-tight">
                 Your Month in Review
               </Heading>
               <Text className="inline-block my-1 bg-fuchsia-600 text-slate-50 text-sm font-normal uppercase tracking-wider rounded-full px-4 py-1">
@@ -51,24 +68,80 @@ const MonthlySummary = ({ dateRangeText }: MonthlySummaryProps) => {
             {/* Spend grid */}
             <Section className="my-4">
               <Row cellSpacing={16} cellPadding={12}>
-                <SpendGridItem category="Good Life" />
-                <SpendGridItem category="Personal" />
+                <SpendGridTile category="Good Life" />
+                <SpendGridTile category="Personal" />
               </Row>
               <Row cellSpacing={16} cellPadding={12} className="-mt-4">
-                <SpendGridItem category="Home" />
-                <SpendGridItem category="Transport" />
+                <SpendGridTile category="Home" />
+                <SpendGridTile category="Transport" />
               </Row>
             </Section>
 
             {/* Top subcategories */}
-            <Section className="my-6">
-              <Text>What you spent on ...</Text>
-            </Section>
+            {categorySpending && categorySpending.length > 0 ? (
+              <Section className="my-6">
+                <Text className="text-3xl font-medium">
+                  What you spent on ...
+                </Text>
+                {categorySpending.map(
+                  ({
+                    category,
+                    categoryName,
+                    parentCategoryName,
+                    absAmount,
+                    amount,
+                  }) => (
+                    <div key={category}>
+                      <div className="flex justify-between mt-4 mb-1">
+                        <Text className="text-md m-0">{categoryName}</Text>
+                        <Text className="text-md m-0">
+                          {formatCurrencyAbsolute(absAmount, amount)}
+                        </Text>
+                      </div>
+                      <SpendBar
+                        barColor={
+                          parentCategoryName ? colours[parentCategoryName] : ''
+                        }
+                        percentage={
+                          (absAmount / categorySpendingMax.absAmount) * 100
+                        }
+                        spending={absAmount}
+                      />
+                    </div>
+                  )
+                )}
+              </Section>
+            ) : null}
 
             {/* Top merchants */}
-            <Section className="my-6">
-              <Text>Who you spend with ...</Text>
-            </Section>
+            {merchantSpending && merchantSpending.length > 0 ? (
+              <Section className="my-6">
+                <Text className="text-3xl font-medium">
+                  Who you spent with ...
+                </Text>
+                {merchantSpending.map(
+                  ({ name, absAmount, amount, parentCategoryName }) => (
+                    <div key={name}>
+                      <div className="flex justify-between mt-4 mb-1">
+                        <Text className="text-md m-0">{name}</Text>
+                        <Text className="text-md m-0">
+                          {formatCurrencyAbsolute(absAmount, amount)}
+                        </Text>
+                      </div>
+                      <SpendBar
+                        barColor={
+                          parentCategoryName ? colours[parentCategoryName] : ''
+                        }
+                        percentage={
+                          (absAmount / merchantSpendingMax.absAmount) * 100
+                        }
+                        spending={absAmount}
+                      />
+                    </div>
+                  )
+                )}
+              </Section>
+            ) : null}
           </Container>
         </Body>
       </Tailwind>
@@ -76,35 +149,46 @@ const MonthlySummary = ({ dateRangeText }: MonthlySummaryProps) => {
   );
 };
 
-const SpendGridItem = ({
+const SpendBar = ({ barColor, percentage, spending }: SpendBarProps) => {
+  return spending === 0 ? (
+    <div
+      className="h-2 rounded-full border border-dashed border-gray-500"
+      style={{ width: '100%' }}
+    />
+  ) : (
+    <div
+      className={cn(
+        'h-2 rounded-full',
+        getColorClassName(
+          barColor ?? ('gray' as AvailableChartColorsKeys),
+          'bg'
+        ),
+        percentage === 0 && 'hidden'
+      )}
+      style={{ width: `${percentage}%` }}
+    />
+  );
+};
+
+const SpendGridTile = ({
   category,
   spending = 0,
   totalSpending = spending,
-}: SpendGridItemProps) => {
-  const barColor = colours[category] ?? 'gray';
+}: SpendGridTileProps) => {
+  const barColor = colours[category];
   const percentage = (spending / totalSpending) * 100;
   return (
-    <Column className="w-1/2 sm:pt-24 border border-solid border-slate-200 rounded-2xl shadow">
+    <Column className="w-1/2 pt-12 sm:pt-24 border border-solid border-slate-200 rounded-2xl shadow">
       <div className="h-full flex flex-col justify-end gap-2">
         <Text className="text-4xl font-medium m-0">
           {formatCurrency(spending)}
         </Text>
         <Text className="text-lg m-0">{category}</Text>
-        {spending === 0 ? (
-          <div
-            className="h-2 rounded-full border border-dashed border-gray-500"
-            style={{ width: '100%' }}
-          />
-        ) : (
-          <div
-            className={cn(
-              'h-2 rounded-full',
-              getColorClassName(barColor as AvailableChartColorsKeys, 'bg'),
-              percentage === 0 && 'hidden'
-            )}
-            style={{ width: `${percentage}%` }}
-          />
-        )}
+        <SpendBar
+          barColor={barColor}
+          spending={spending}
+          percentage={percentage}
+        />
       </div>
     </Column>
   );
@@ -112,6 +196,86 @@ const SpendGridItem = ({
 
 MonthlySummary.PreviewProps = {
   dateRangeText: 'March 2025',
+  categorySpending: [
+    {
+      category: 'homeware-and-appliances',
+      categoryName: 'Homeware & Appliances',
+      parentCategory: 'home',
+      parentCategoryName: 'Home',
+      amount: -888.87,
+      absAmount: 888.87,
+      transactions: 6,
+    },
+    {
+      category: 'restaurants-and-cafes',
+      categoryName: 'Restaurants & Cafes',
+      parentCategory: 'good-life',
+      parentCategoryName: 'Good Life',
+      amount: -844.24,
+      absAmount: 844.24,
+      transactions: 19,
+    },
+    {
+      category: 'public-transport',
+      categoryName: 'Public Transport',
+      parentCategory: 'transport',
+      parentCategoryName: 'Transport',
+      amount: -806.82,
+      absAmount: 806.82,
+      transactions: 79,
+    },
+    {
+      category: 'fitness-and-wellbeing',
+      categoryName: 'Fitness & Wellbeing',
+      parentCategory: 'personal',
+      parentCategoryName: 'Personal',
+      amount: 181.77,
+      absAmount: 181.77,
+      transactions: 53,
+    },
+  ],
+  merchantSpending: [
+    {
+      name: 'Investments Co.',
+      absAmount: 500,
+      amount: -500,
+      transactions: 2,
+      category: 'investments',
+      categoryName: 'Investments',
+      parentCategory: 'personal',
+      parentCategoryName: 'Personal',
+    },
+    {
+      name: 'Fancy Dining',
+      absAmount: 200,
+      amount: -200,
+      transactions: 1,
+      category: 'restaurants-and-cafes',
+      categoryName: 'Restaurants & Cafes',
+      parentCategory: 'good-life',
+      parentCategoryName: 'Good Life',
+    },
+    {
+      name: 'Fitness Club',
+      absAmount: 45.0,
+      amount: -45.0,
+      transactions: 1,
+      category: 'fitness-and-wellbeing',
+      categoryName: 'Fitness & Wellbeing',
+      parentCategory: 'personal',
+      parentCategoryName: 'Personal',
+    },
+    {
+      name: 'Public Transport Authority',
+      absAmount: 40.0,
+      amount: -40.0,
+      transactions: 2,
+      category: 'public-transport',
+      categoryName: 'Public Transport',
+      parentCategory: 'transport',
+      parentCategoryName: 'Transport',
+    },
+  ],
 };
 
 export default MonthlySummary;
